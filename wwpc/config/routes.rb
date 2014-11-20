7f6b6eab2f5578b2a7ddb196c5715226478cd482
Alexander Claes <alexander.claes10@gmail.com>
2014-11-20 20:17:31 +0100

    setup devise and routes

--- wwpc/config/routes.rb
+++ wwpc/config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :user
+
+  root 'home#index'
+
+  get 'home/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
