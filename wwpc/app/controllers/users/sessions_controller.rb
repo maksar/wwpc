ee80dadd57fffa3076c781c7babf39d491a70b50
lextoc <alexander.claes10@gmail.com>
2014-12-03 19:13:38 +0100

    show logged in user

--- wwpc/app/controllers/users/sessions_controller.rb
+++ wwpc/app/controllers/users/sessions_controller.rb
@@ -19,6 +19,10 @@ before_filter :configure_sign_in_params, only: [:create]
 
   protected
 
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :activated, :email, :password, :password_confirmation) }
+  end
+
   # You can put the params you want to permit in the empty array.
   def configure_sign_in_params
     devise_parameter_sanitizer.for(:sign_in) << :attribute
